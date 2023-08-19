fc2be9cf73263fd4d986bedbbc2afbeb8703fc44
Atro <103967835+atrogpx@users.noreply.github.com>
2023-08-19 20:07:30 +0200

    works for 2 numbers and an operator

--- javascript.js
+++ javascript.js
@@ -1,34 +1,38 @@
-let operand1;
-let operator;
-let operand2;
+let dispVal = "";
 
 function add(num1, num2) {
-    return num1 + num2;
+    dispVal = "";
+    return populate(num1 + num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    dispVal = "";
+    return populate(num1 - num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    dispVal = "";
+    return populate(num1 * num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    dispVal = "";
+    return populate(num1 / num2);
 }
 
-function operate(operator, num1, num2) {
+function operate() {
+    let op = dispVal.split(" ");
+    let num1 = op[0];
+    let operator = op[1];
+    let num2 = op[2];
     if (operator == '+') return add(num1, num2);
     if (operator == '-') return subtract(num1, num2);
     if (operator == '*') return multiply(num1, num2);
     if (operator == '/') return divide(num1, num2);
 }
 
-digits = document.querySelectorAll(".digit");
-
-for (i = 0; i < digits.length; i++) {
-    digits[i].addEventListener("click", (e) => console.log(e.textContent))
+function populate(str) {
+    let populator = document.querySelector(".populator");
+    dispVal = dispVal.concat(str);
+    populator.textContent = dispVal;
 }
-
-function populate() {}
